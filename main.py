d98c8018de983fcb8a3af17c4b11b9bfe2d473fa
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-03-27 22:42:51 +0800

    chore: add test calls

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from controller import Controller, GPIO_MAPPING as port , PORT_MODE as mode, PORT_STATE as state
-
+from reader import UltrasonicSensor
 if __name__ == "__main__":
     controller = Controller()
     controller.toggle_pin(port.GATE_TRIGGER,state.HIGH);
+
+    reader = UltrasonicSensor("deez",69_420) 
+    reader.read() 
+    print(f":: [DISTANCE] : {reader.get_reading()}")
