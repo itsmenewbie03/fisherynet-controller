167fbfa8681d541c60074cc8b40e73a86f80bc41
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-03-28 17:02:59 +0800

    wip: controller implementations

--- controller.py
+++ controller.py
@@ -1,8 +1,8 @@
 from enum import Enum
-from pyA20.gpio import gpio # pyright: ignore
+from pyA20.gpio import gpio, port # pyright: ignore
 
 class GPIO_MAPPING(Enum):
-    GATE_TRIGGER = "PA7"
+    GATE_TRIGGER = port.PA7
     # NOTE: add more GPIO as needed
     
 class PORT_MODE(Enum):
@@ -22,6 +22,7 @@ class Controller:
     def __init__(self) -> None:
         self.prod = self.__on_armbian()
         self.__prepare_pins()
+        print(f":: [ENV] {'PROD' if self.prod else 'TEST'}")
     
     def __on_armbian(self) -> bool:
         """
@@ -39,24 +40,34 @@ class Controller:
         e.g setting port.PA7 as OUTPUT. The implementation of this
         would be heavily reliant on the design of the system.
         """
+        print(f":: [PREPARE_PINS] ...") 
         # TODO: implement the configuration needed
         if self.prod:
+            self.__set_pin_mode(GPIO_MAPPING.GATE_TRIGGER,PORT_MODE.OUTPUT)
             return
-        print(f":: [PREPARE_PINS] ...") 
         # NOTE: a dummy setup 
         self.__set_pin_mode(GPIO_MAPPING.GATE_TRIGGER,PORT_MODE.OUTPUT)
         
+    def __check_pin_mode(self,port: GPIO_MAPPING) -> Optional[int]:
+        if self.prod:
+            status = gpio.getcfg(port.value)
+            return status
+        print(f":: [PIN_CHECK] ...")
+         
     def __set_pin_mode(self,pin: GPIO_MAPPING,mode: PORT_MODE) -> None:
         """
-        This function is for setting the pin mode conveniently.
+        This function is for setting the PIN MODE not STATUS conveniently.
         This will wrap the function provided by the module.
         """
         if self.prod:
+            gpio.setcfg(pin.value,mode.value)
             # NOTE: implement the toggle
             # we are coding on a Laptop not on the Actual Orange PI one
             # so we gotta make the framework for it first
             return
         print(f":: [PORT_MODE_SET] port: {pin}|{pin.value} mode: {mode}|{mode.value}")         
+    def check_pins(self,pins: list[GPIO_MAPPING]) -> None:
+        pass
         
     def toggle_pin(self,pin: GPIO_MAPPING, mode: PORT_STATE) -> None:
         if self.prod:
