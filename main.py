4d90ff56e0c981cb73adcf40863e22c03a3113ce
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-03-28 23:32:22 +0800

    feat: add test to detector

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from controller import Controller, GPIO_MAPPING as port , PORT_MODE as mode, PORT_STATE as state
-from reader import UltrasonicSensor
+from detector import Detector
+from reader import Camera, UltrasonicSensor
+
 if __name__ == "__main__":
     controller = Controller()
     controller.check_pins([port.GATE_TRIGGER])
     controller.toggle_pin(port.GATE_TRIGGER,state.HIGH);
     gate_trigger_state = controller.read_pin(port.GATE_TRIGGER)
     print(f":: [GATE_TRIGGER] : {gate_trigger_state}")
-
+    
     reader = UltrasonicSensor("/dev/ttyUSB0",115_200) 
     reader.read() 
-    print(f":: [DISTANCE] : {reader.get_reading()}")
+    distance = reader.get_reading()
+    print(f":: [DISTANCE] : {distance}")
+    
+    camera = Camera()
+    camera.read()
+    image = camera.get_reading()
+    
+    detector = Detector(1.5)
+    # NOTE: we know that image is indeed a bytearray 
+    # we will make pyright shut up
+    # This issue with types kinda tells me that I should do some refactoring
+    est_size = detector.detect_size(image,float(distance)) # pyright: ignore
+    print(f":: [DETECTOR] Estimated Size: {est_size}")
