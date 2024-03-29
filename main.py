1aca9f0f17d500adb04a246cca5923eb3da0bab6
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-03-29 23:16:16 +0800

    feat: add test for connector

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from calibrator import Calibrator
-from connector import Connector
+from connector import CONFIGS, Connector
 from controller import Controller, GPIO_MAPPING as port , PORT_MODE as mode, PORT_STATE as state
 from detector import Detector
 from reader import Camera, UltrasonicSensor
@@ -10,16 +10,16 @@ if __name__ == "__main__":
     controller.toggle_pin(port.GATE_TRIGGER,state.HIGH);
     gate_trigger_state = controller.read_pin(port.GATE_TRIGGER)
     print(f":: [GATE_TRIGGER] : {gate_trigger_state}")
-    
+
     reader = UltrasonicSensor("/dev/ttyUSB0",115_200) 
     reader.read() 
     distance = reader.get_reading()
     print(f":: [DISTANCE] : {distance}")
-    
+
     camera = Camera()
     camera.read()
     image = camera.get_reading()
-    
+
     detector = Detector(1.5)
     # NOTE: we know that image is indeed a bytearray 
     # we will make pyright shut up
@@ -29,5 +29,8 @@ if __name__ == "__main__":
 
     connector = Connector()
     connector.calibrator = Calibrator()
-    connector.start()
+    # connector.start()
+    connector.get_config(CONFIGS.MIN_FISH_SIZE);
+    # connector.stop()
+    
     
