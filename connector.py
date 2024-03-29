b37a94de984e4d55b54bcb03187c0b51721d5b40
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-03-29 19:05:10 +0800

    feat: implement connector
    This class is responsible for communicating with the frontend through an
    MQTT connection. We will utilize topics to have an easy way of parsing
    the payload. We can implement a custom handler for each topic and based
    on the payload perform the action.

---
+++ connector.py
@@ -0,0 +1,51 @@
+import paho.mqtt.client as mqtt
+
+from calibrator import Calibrator
+
+class Connector:
+    broker = "mqtt.eclipseprojects.io"
+    port = 1883
+    keepalive = 60
+    client: mqtt.Client 
+    calibrator: Calibrator
+    
+    def __on_message(self,client, userdata, msg) -> None:
+        # print(msg.topic+" "+str(msg.payload))
+        # INFO: call the internal message handler
+        self.__message_handler(msg.topic,msg.payload)
+        
+    def __on_connect(self,client, userdata, flags, reason_code, properties) -> None:
+        print(f":: [CONNECTOR] Connected with result code {reason_code}")
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe("FISHERYNET|COMMANDS")
+        
+    def __message_handler(self, topic: bytes, payload: bytes) -> None:
+        """
+        Handle the massage according to the topic in which they are sent 
+        """
+        match str(topic):
+            case "FISHERYNET|COMMANDS":
+                self.__command_handler(payload)
+            case _:
+                print(f":: [MESSAGE_HANDER] handler for topic {topic} is not yet implemented.")
+                
+    def __command_handler(self, command: bytes):
+        match command:
+            case b"START_CAMERA_STREAM":
+                # TODO: implement the actual handler
+                print(f":: [COMMAND_HANDLER]  starting camera stream")
+            case b"START_DETECTION_CALIBRATION":
+                print(f":: [COMMAND_HANDLER]  detection calibration started")
+                self.calibrator.calibrate_detection()
+            case _:
+                print(f":: [COMMAND_HANDLER] handler for command {str(command)} is not yet implemented.")
+                
+    def __init__(self) -> None:
+        self.client =  mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)    # pyright: ignore 
+        self.client.on_connect = self.__on_connect
+        self.client.on_message = self.__on_message
+        self.client.connect(self.broker,self.port,self.keepalive)    
+        
+    def start(self) -> None:
+        self.client.loop_forever()
