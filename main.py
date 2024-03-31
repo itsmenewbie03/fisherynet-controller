1a47338a8c10aa0d5e01154e65834c0b90682600
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-03-31 20:54:29 +0800

    chore: testing pins

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ if __name__ == "__main__":
     controller = Controller()
     controller.check_pins([port.GATE_TRIGGER])
     print(f":: [BLINK_TEST] performing a blink test...")
-    for x in range(5):
+    for x in range(10):
         controller.toggle_pin(port.GATE_TRIGGER,state.HIGH if x%2==0 else state.LOW);
-        controller.toggle_pin(port.EXTRA_1,state.HIGH if x%2==0 else state.LOW);
-        controller.toggle_pin(port.EXTRA_2,state.HIGH if x%2==0 else state.LOW);
+        # controller.toggle_pin(port.EXTRA_1,state.HIGH if x%2==0 else state.LOW);
+        # controller.toggle_pin(port.EXTRA_2,state.HIGH if x%2==0 else state.LOW);
         sleep(1)
+        
     controller.toggle_pin(port.GATE_TRIGGER,state.LOW);
+    # controller.toggle_pin(port.EXTRA_1,state.LOW);
+    # controller.toggle_pin(port.EXTRA_2,state.LOW);
+    
     gate_trigger_state = controller.read_pin(port.GATE_TRIGGER)
     print(f":: [GATE_TRIGGER] : {gate_trigger_state}")
 
