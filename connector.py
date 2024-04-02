9069f0972f165da904fa632e6eac9a5e50c06797
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-04-02 19:02:30 +0800

    fix: config_request are now properly working
    This fixes the issue where the config requests can't be seen or the
    config requests causes the main network loop the terminate.

--- connector.py
+++ connector.py
@@ -1,38 +1,49 @@
+from typing import Any
 import paho.mqtt.client as mqtt
 
 from time import sleep
 from calibrator import Calibrator
-from enum import Enum
 from controller import GPIO_MAPPING, PORT_STATE, Controller
-    
+from enum import Enum
+
 class CONFIGS(Enum):
     MIN_FISH_SIZE = "min_fish_size"    
-
+    
 class Connector:
+    is_config_handler = False
     broker = "mqtt.eclipseprojects.io"
     port = 1883
     keepalive = 60
     client: mqtt.Client 
     calibrator: Calibrator
     controller: Controller
+    # NOTE: making this Any to avoid circular imports
+    config_handler: Any 
     # NOTE: we store the config here because I think having `config`
     config: dict = {}
+    stopped: bool = False
     
-    def __init__(self,controller: Controller) -> None:
+    def __init__(self,controller: Controller,config_handler: Any) -> None:
         self.client =  mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)    # pyright: ignore 
         self.client.on_connect = self.__on_connect
         self.client.on_message = self.__on_message
         self.client.connect(self.broker,self.port,self.keepalive)    
         self.controller = controller
+        self.config_handler = config_handler
         
     def __on_message(self,client, userdata, msg) -> None:
         # INFO: call the internal message handler
         self.__message_handler(msg.topic,msg.payload)
         
     def __on_connect(self,client, userdata, flags, reason_code, properties) -> None:
-        print(f":: [CONNECTOR] Connected with result code {reason_code}")
-        self.client.subscribe("FISHERYNET|COMMANDS")
-        self.client.subscribe("FISHERYNET|TOGGLE_PORT")
+        print(f":: [{'GENERIC' if not self.is_config_handler else 'CONFIG_HANDLER'}_CONNECTOR] Connected with result code {reason_code}")
+        if not self.is_config_handler:
+            self.client.subscribe("FISHERYNET|COMMANDS")
+            self.client.subscribe("FISHERYNET|TOGGLE_PORT")
+            
+            # NOTE: if connector is not a config_hanlder we won't subscribe to CONFIG_RESPONSE
+            # early return 
+            return
         self.client.subscribe("FISHERYNET|CONFIG_RESPONSE")
         
     def __message_handler(self, topic: bytes, payload: bytes) -> None:
@@ -64,7 +75,7 @@ class Connector:
                 print(f":: [COMMAND_HANDLER]  detection calibration started")
                 est_size = self.calibrator.calibrate_detection()
                 # call the get config to compare the estimated size with the minimum fish size
-                min_fish_size = self.get_config(CONFIGS.MIN_FISH_SIZE)
+                min_fish_size = self.config_handler.get_config(CONFIGS.MIN_FISH_SIZE)
                 print(f":: [COMMAND_HANDLER]  estimated size: {est_size}, minimum fish size: {min_fish_size}")
                 print(f":: [COMMAND_HANDLER]  the fish is {'big' if est_size >= min_fish_size else 'small'}")
                 self.client.publish("FISHERYNET|CALIBRATION_RESPONSE",f"est_size={est_size}") 
@@ -100,17 +111,31 @@ class Connector:
         # NOTE: we subscribe because we wil only be interested in CONFIG_RESPONSE
         # when we actually request for a configuration
         self.client.publish("FISHERYNET|CONFIG_REQUEST",f"{config_name.value}")
+        
+        retry_count = 0
         while not config_name.value in self.config:
+            if retry_count > 10:
+                print(":: [GET_CONFIG] Maximum retries reached")
+                raise Exception("Maximum retries reached")
             print(":: [GET_CONFIG] Waiting for response...")
-            sleep(1)
+            retry_count += 1
+            sleep(0.5)
+            
         self.client.loop_stop()
         config_value = self.config[config_name.value]
-        print(f":: [GET_CONFIG] Configuration: {config_value}")
+        print(f":: [GET_CONFIG_SUCCESS] Configuration: {config_value}")
+        self.config.pop(config_name.value)
         return config_value
     
     def start(self) -> None:
-        print(":: [MQTT_LISTENER] started listening...")
-        self.client.loop_forever()
+        print(":: [MQTT_LISTENER] main listener started! ")
+        self.client.loop_start()
+        while True:
+            if self.stopped:
+                print(":: [MQTT_LISTENER] main listener stopped! ")
+                self.client.disconnect()
+                break
+        self.client.loop_stop()
         
     def stop(self) -> None:
-        self.client.disconnect()
+        self.stopped = True
