3d50787fe1aedd6da2cde6652e88745e684187b2
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-04-02 19:04:40 +0800

    refactor: temporarily disabled the image output

--- detector.py
+++ detector.py
@@ -1,7 +1,6 @@
 from typing import Optional
 import cv2 
 import numpy as np
-from datetime import datetime
 
 class Detector:
     # TODO: add actual CF
@@ -41,7 +40,7 @@ class Detector:
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2) 
         # INFO: write the image to a file 
         # used for visualizing how the detection went
-        cv2.imwrite(f'sized_{datetime.now()}.png', img) 
+        # cv2.imwrite(f'sized_{datetime.now()}.png', img) 
         area = cv2.contourArea(largest_countour) 
         return area
 
