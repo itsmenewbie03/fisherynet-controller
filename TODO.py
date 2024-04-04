a7b590c04171734c66741cead896947be25c303b
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-04-04 21:50:19 +0800

    chore: move TODO.py to TODO.md

--- TODO.py
+++
@@ -1,5 +0,0 @@
-# TODO: implement a reporter class
-# this will handle sending reports to the dashboard
-# it will publish the reports to the FISHERYNET|REPORTS topic
-# the format is yet to be decided but it should be easy to parse in the dashboard
-# this class will be typically called after a successfull detection
